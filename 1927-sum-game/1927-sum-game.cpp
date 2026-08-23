1e8768640408ac3ab829cd2af3efb1c328d929ea
MOHAMMAD AMAAN WARIS <146476631+amaanwaris@users.noreply.github.com>
2026-08-23 23:17:32 +0530

    Time: 3 ms (66.67%), Space: 14 MB (67.83%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+
+        int sum1 = 0, sum2 = 0;
+        int q1 = 0, q2 = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num[i] == '?')
+                q1++;
+            else
+                sum1 += num[i] - '0';
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num[i] == '?')
+                q2++;
+            else
+                sum2 += num[i] - '0';
+        }
+
+        int diff = sum1 - sum2;
+        int qdiff = q1 - q2;
+
+        // Odd difference in '?' count
+        if (qdiff % 2 != 0)
+            return true;
+
+        // Bob can win only if equality is possible
+        return diff != -9 * qdiff / 2;
+    }
+};
